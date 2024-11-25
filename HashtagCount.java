5b81dc4659f1b9b003e0e8e99329fc5e933945fa
Ananthakumar Vithurshanan <lithuvithu1109@gmail.com>
2024-11-26 00:01:03 +0530

    Adding find popular hastage finding hadoop

---
+++ HashtagCount.java
@@ -0,0 +1,55 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+public class HashtagCount {
+    public static class HashtagMapper extends Mapper<Object, Text, Text, IntWritable> {
+        private final static IntWritable one = new IntWritable(1);
+        private Text hashtag = new Text();
+
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            String[] words = value.toString().split("\\s+");
+            for (String word : words) {
+                if (word.startsWith("#")) {
+                    hashtag.set(word);
+                    context.write(hashtag, one);
+                }
+            }
+        }
+    }
+
+    public static class HashtagReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "hashtag count");
+        job.setJarByClass(HashtagCount.class);
+        job.setMapperClass(HashtagMapper.class);
+        job.setCombinerClass(HashtagReducer.class);
+        job.setReducerClass(HashtagReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
